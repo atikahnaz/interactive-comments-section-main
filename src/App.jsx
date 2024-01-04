a0c584faa142795a0ecb6fffb37f452c4925a321
Atikah <atikahnazir6@gmail.com>
2024-01-04 18:24:46 +0800

    callback function to save data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CommentsBox from "./componets/CommentsBox";
-import Data from "../data.json";
+import CommentsData from "../data.json";
 
 function App() {
+  const [Data, setData] = useState(CommentsData);
+  const [idNew, setIdNew] = useState(4);
+
   // callback function to save text
-  const addTextData = () => {
+  const addTextComment = () => {
     console.log("hy");
   };
+
+  //callback function to save comments
+  const addCommentsText = (item) => {
+    console.log("send comments");
+    console.log(item);
+    // add id to comments
+    setIdNew((idNo) => {
+      const newComments = { ...item, id: idNo + 1 };
+      console.log(newComments);
+
+      setData({ ...Data, comments: [...Data.comments, newComments] });
+      return idNo + 1;
+    });
+  };
+
+  useEffect(() => {
+    console.log(Data);
+  }, [Data]);
   return (
     <>
       <div className="">
-        <CommentsBox Data={Data} addText={addTextData} />
+        <CommentsBox
+          Data={Data}
+          addText={addTextComment}
+          saveCommentsText={addCommentsText}
+        />
       </div>
     </>
   );
