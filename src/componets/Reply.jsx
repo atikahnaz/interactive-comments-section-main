9403651ca785bb5bb4179855999af9fdddec6a45
Atikah <atikahnazir6@gmail.com>
2024-01-08 18:34:43 +0800

    show reply

--- src/componets/Reply.jsx
+++ src/componets/Reply.jsx
@@ -8,6 +8,7 @@ export default function Reply({
 }) {
   const [reply, setReply] = useState("@" + usernameName);
   const [replyText, setReplyText] = useState("");
+  const [showReply, setShowReply] = useState("true");
   const [replyData, setReplyData] = useState({
     id: null,
     content: replyText,
@@ -33,29 +34,32 @@ export default function Reply({
   const sendReply = () => {
     textReply(replyData, idToReply);
     setReplyData({ ...replyData, content: "" });
+    setShowReply(false);
   };
 
   return (
     <>
-      <div className=" bg-fe-white mb-4 p-4 rounded-lg font-feRubik">
-        <input
-          type="text"
-          value={replyData.content}
-          onChange={InputComment}
-          className="border border-fe-light-grayish-blue w-full h-28 p-4 mb-4"
-        />
-        <div className="flex justify-between items-center">
-          <div>
-            <img className="w-7 h-7" src={currentUser.image.png} alt="" />
-          </div>
-          <div
-            className="bg-fe-moderate-blue px-6 py-3 text-fe-white text-base rounded-lg font-medium"
-            onClick={sendReply}
-          >
-            SEND
+      {showReply && (
+        <div className=" bg-fe-white mb-4 p-4 rounded-lg font-feRubik">
+          <input
+            type="text"
+            value={replyData.content}
+            onChange={InputComment}
+            className="border border-fe-light-grayish-blue w-full h-28 p-4 mb-4"
+          />
+          <div className="flex justify-between items-center">
+            <div>
+              <img className="w-7 h-7" src={currentUser.image.png} alt="" />
+            </div>
+            <div
+              className="bg-fe-moderate-blue px-6 py-3 text-fe-white text-base rounded-lg font-medium"
+              onClick={sendReply}
+            >
+              SEND
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
