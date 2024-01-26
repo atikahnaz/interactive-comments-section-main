0a618424417f63d045dda4426b28f7e32cb93066
Atikah <atikahnazir6@gmail.com>
2024-01-26 17:39:40 +0800

    delete popup

--- src/componets/Delete.jsx
+++ src/componets/Delete.jsx
@@ -1,20 +1,35 @@
-export default function DeleteComment() {
-  console.log("try delete");
+export default function DeleteComment({ id, idComment, cancelDelete }) {
+  console.log("try delete" + id);
+  const deleteId = (id) => idComment(id);
+  const handleClickCancel = () => {
+    cancelDelete();
+  };
+
   return (
     <>
       <div className="fixed w-full h-full z-0  top-0 left-0 ">
         <div className="flex w-full h-full z-10 justify-center items-center bg-fe-dark-blue bg-opacity-50">
-          <div className=" bg-fe-white p-4 w-3/4 h-1/4">
-            <div>Delete comment</div>
-            <div>
-              Are you sure want to delete this comment? This will remove the
-              comment and can't be undone
+          <div className=" bg-fe-white p-6 w-fit mx-4 rounded-md ">
+            <div className="font-bold pb-2 text-fe-dark-blue text-xl">
+              Delete comment
             </div>
-            <div className="flex justify-between">
-              <button className=" bg-fe-grayish-Blue px-3 py-2">
+            <div className=" text-fe-grayish-Blue leading-normal  text-lg">
+              Are you sure want to delete this comment? This will remove the
+              comment and can't be undone.
+            </div>
+            <div className="flex justify-between pt-3">
+              <button
+                className=" bg-fe-grayish-Blue px-5 py-3 rounded-md text-fe-white font-medium text-base"
+                onClick={handleClickCancel}
+              >
                 NO, CANCEL
               </button>
-              <button className=" bg-fe-soft-red px-3 py-2">YES, DELETE</button>
+              <button
+                className=" bg-fe-soft-red px-5 py-3 rounded-md text-fe-white font-medium text-base"
+                onClick={() => deleteId(id)}
+              >
+                YES, DELETE
+              </button>
             </div>
           </div>
         </div>
